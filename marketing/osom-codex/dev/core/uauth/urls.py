cff45c8798ab8803c9948d0820fa4a1c2fd7cc2f
Gustas <gustas@indeform.com>
2025-03-10 09:45:16 +0200

    Authentication startup functions

--- marketing/osom-codex/dev/core/uauth/urls.py
+++ marketing/osom-codex/dev/core/uauth/urls.py
@@ -5,4 +5,6 @@ app_name = "core.uauth"
 
 urlpatterns = [
     path("logout", auth_views.LogoutView.as_view(), name="logout"),
+    path("login", auth_views.LoginView.as_view(template_name="core/uauth/login.html"), name="login"),	
+    path("register", auth_views.LoginView.as_view(template_name="core/uauth/register.html"), name="register"),
 ]
--- marketing/osom-codex/dev/parea/urls.py
+++ marketing/osom-codex/dev/parea/urls.py
@@ -1,12 +1,16 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
+from core.uauth.views import views as uauth_views
 
 app_name = "parea"
 
+# urlpatterns = [
+#     path(
+#         "",
+#         auth_views.LoginView.as_view(redirect_authenticated_user=True, template_name="parea/index.html"),
+#         name="index",
+#     ),
+# ]
 urlpatterns = [
-    path(
-        "",
-        auth_views.LoginView.as_view(redirect_authenticated_user=True, template_name="parea/index.html"),
-        name="index",
-    ),
+    path("", uauth_views.login_page, name="index"),
 ]
