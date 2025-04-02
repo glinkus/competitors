1cb3475bfe2546bd159c6d6f590d1ee0a3d7a96b
Gustas Linkus <gustaslink@idf-betelgeuse>
2025-04-02 11:10:18 +0300

    Added a chart for text tones visualization

--- marketing/osom-codex/dev/modules/analysis/views/analysis_overview_view.py
+++ marketing/osom-codex/dev/modules/analysis/views/analysis_overview_view.py
@@ -1,14 +1,12 @@
 from django.views.generic import TemplateView
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
-from modules.analysis.models import Page, ExtractedKeyword, Website
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import torch
-from transformers import pipeline
+from modules.analysis.models import Page, Website
+from statistics import median
+
 
 class OverviewView(TemplateView):
     template_name = "modules/analysis/overview.html"
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -16,30 +14,43 @@ class OverviewView(TemplateView):
         website = get_object_or_404(Website, id=website_id)
 
         pages = Page.objects.filter(website_id=website_id).exclude(text_types__isnull=True)
-        average_tones = self.calculate_average_tones(pages)
+        median_tones, most_technical_url, least_technical_url = self.calculate_median_tones(pages)
 
         context.update({
             "website": website,
-            "average_tones": average_tones,
+            "median_tones": median_tones,
+            "tone_labels": list(median_tones.keys()),
+            "tone_data": list(median_tones.values()),
+            "most_technical_url": most_technical_url,
+            "least_technical_url": least_technical_url,
         })
         return context
-    
-    def calculate_average_tones(self, pages):
-        tone_totals = {}
-        tone_counts = {}
+
+    def calculate_median_tones(self, pages):
+        tone_scores = {}
+        technical_scores = []
 
         for page in pages:
             if not page.text_types:
                 continue
 
             for tone, score in page.text_types.items():
-                tone_totals[tone] = tone_totals.get(tone, 0) + score
-                tone_counts[tone] = tone_counts.get(tone, 0) + 1
+                tone_scores.setdefault(tone, []).append(score)
 
-        average_tones = {
-            tone: round(tone_totals[tone] / tone_counts[tone], 2)
-            for tone in tone_totals
+            technical_score = page.text_types.get("technical")
+            if technical_score is not None:
+                technical_scores.append((technical_score, page.url))
+
+        median_tones = {
+            tone: round(median(scores), 2)
+            for tone, scores in tone_scores.items()
         }
 
-        return average_tones
+        most_technical_url = None
+        least_technical_url = None
 
+        if technical_scores:
+            most_technical_url = max(technical_scores, key=lambda x: x[0])[1]
+            least_technical_url = min(technical_scores, key=lambda x: x[0])[1]
+
+        return median_tones, most_technical_url, least_technical_url
